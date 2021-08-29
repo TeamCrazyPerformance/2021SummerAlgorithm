8ae4c72a8fdcd0c5a9b488e417966e05280a0102
yongrae <kyr9389@naver.com>
2021-08-29 22:57:32 +0900

    UPDATE statcard component added
    Signed-off-by: yongrae <kyr9389@naver.com>

--- src/component/StatCard.jsx
+++ src/component/StatCard.jsx
@@ -4,7 +4,10 @@ import './StatCard.css';
 function StatCard({name, tier, solved, classes}) {
     return (
         <div className="card">
-
+            <p className="cardName">{name}</p>
+            <p className="cardTier">{tier}</p>
+            <p className="cardSolved">{solved}</p>
+            <p className="cardClasses">{classes}</p>
         </div>
     )
 }
